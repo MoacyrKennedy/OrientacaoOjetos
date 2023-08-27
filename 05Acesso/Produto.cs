6d745897406d27c405b0f810193472db30667698
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2023-08-27 12:29:24 -0300

    metodos

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,9 @@
+
+class Produto
+{
+    /*
+        public   -> Atributos e métodos visíveis em qualquer classe
+        private  -> Atributos e métodos visíveis apenas na classe onde são criados
+        protected -> Atributos e métodos visíveis apenas na classe onde são criados e nas classes derivadas
+    */    
+}
